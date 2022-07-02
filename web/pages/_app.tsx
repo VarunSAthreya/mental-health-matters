1b98b13d9d9f7bf703dce0b8d05b184ef7977434
Sandeep.M <sandeep.m24.rathnam@gmail.com>
2022-07-02 23:11:15 +0530

    Added More Pages and Fixed Few Issues

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,18 +1,13 @@
-import { extendTheme, NativeBaseProvider, theme as nbTheme } from "native-base";
+import { NativeBaseProvider } from "native-base";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import theme from '../styles/theme';
 import React from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-    const theme = extendTheme({
-        colors: {
-            primary: nbTheme.colors.violet,
-        },
-    });
-
     return (
-        <ChakraProvider>
-            <NativeBaseProvider theme={theme} isSSR>
+        <ChakraProvider theme={theme}>
+            <NativeBaseProvider>
                 <Component {...pageProps} />
             </NativeBaseProvider>
         </ChakraProvider>
---
+++ web/styles/theme.tsx
@@ -0,0 +1,18 @@
+import { extendTheme } from '@chakra-ui/react';
+import { mode } from '@chakra-ui/theme-tools';
+
+const theme = extendTheme({
+    styles: {
+        global: (props: any) => ({
+            body: {
+                bg: mode('#f8f9fa', '#171120')(props),
+            },
+        }),
+    },
+    config: {
+        initialColorMode: 'dark',
+        useSystemColorMode: false,
+    },
+});
+
+export default theme;
