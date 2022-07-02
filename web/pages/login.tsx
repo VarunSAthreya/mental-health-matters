88814f0d49065b4cc86d024a5313d98fd8bb528a
Sandeep.M <sandeep.m24.rathnam@gmail.com>
2022-07-02 22:20:25 +0530

    Changed login page

--- web/pages/login.tsx
+++ web/pages/login.tsx
@@ -1,121 +1,137 @@
 import {
-    Box,
-    Button,
-    Center,
-    FormControl,
-    Heading,
-    HStack,
-    Input,
-    Link,
-    Text,
-    VStack,
-} from "native-base";
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  HStack,
+  InputRightElement,
+  Stack,
+  Button,
+  Heading,
+  Text,
+  useColorModeValue,
+  Link,
+} from "@chakra-ui/react";
+import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
 import { useRouter } from "next/router";
 import React, { useState } from "react";
 import { useAuth } from "../hooks/auth";
+import Layout from "../components/UI/Layout";
 
 const Login = () => {
-    const router = useRouter();
-    const { user, login } = useAuth();
-    const [data, setData] = useState({
-        email: "",
-        password: "",
-    });
+  const router = useRouter();
+  const [showPassword, setShowPassword] = useState(false);
+  const [signUp, setSignUp] = useState(false);
+  const { user, login } = useAuth();
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
 
-    const handleLogin = async (e: any) => {
-        e.preventDefault();
+  const handleLogin = async (e: any) => {
+    e.preventDefault();
 
-        console.log(user);
-        try {
-            await login(data.email, data.password);
-            router.push("/dashboard");
-        } catch (err) {
-            console.log(err);
-        }
-    };
+    console.log(user);
+    try {
+      await login(data.email, data.password);
+      router.push("/dashboard");
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-    return (
-        <Center w="100%">
-            <Box safeArea p="2" py="8" w="90%" maxW="290">
-                <Heading
-                    size="lg"
-                    fontWeight="600"
-                    color="coolGray.800"
-                    _dark={{
-                        color: "warmGray.50",
-                    }}
-                >
-                    Welcome
-                </Heading>
-                <Heading
-                    mt="1"
-                    _dark={{
-                        color: "warmGray.200",
-                    }}
-                    color="coolGray.600"
-                    fontWeight="medium"
-                    size="xs"
-                >
-                    Sign in to continue!
-                </Heading>
-
-                <VStack space={3} mt="5">
-                    <FormControl>
-                        <FormControl.Label>Email ID</FormControl.Label>
-                        <Input
-                            onChange={(e) =>
-                                setData({ ...data, email: e.target.value })
-                            }
-                        />
+  return (
+    <Layout title="MHM | Login">
+      <Flex
+        minH={"100vh"}
+        align={"center"}
+        justify={"center"}
+        bg={useColorModeValue("gray.50", "gray.800")}
+      >
+        <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+          <Stack align={"center"}>
+            <Heading fontSize={"4xl"} textAlign={"center"}>
+              {signUp ? "Sign Up" : "Login"}
+            </Heading>
+            <Text fontSize={"lg"} color={"gray.600"}>
+              to enjoy all of our cool features ✌️
+            </Text>
+          </Stack>
+          <Box
+            rounded={"lg"}
+            bg={useColorModeValue("white", "gray.700")}
+            boxShadow={"lg"}
+            p={8}
+          >
+            <Stack spacing={4}>
+              {signUp && (
+                <HStack>
+                  <Box>
+                    <FormControl id="firstName" isRequired>
+                      <FormLabel>First Name</FormLabel>
+                      <Input type="text" />
                     </FormControl>
-                    <FormControl>
-                        <FormControl.Label>Password</FormControl.Label>
-                        <Input
-                            type="password"
-                            onChange={(e) =>
-                                setData({ ...data, password: e.target.value })
-                            }
-                        />
-                        <Link
-                            _text={{
-                                fontSize: "xs",
-                                fontWeight: "500",
-                                color: "indigo.500",
-                            }}
-                            alignSelf="flex-end"
-                            mt="1"
-                        >
-                            Forget Password?
-                        </Link>
+                  </Box>
+                  <Box>
+                    <FormControl id="lastName">
+                      <FormLabel>Last Name</FormLabel>
+                      <Input type="text" />
                     </FormControl>
-                    <Button mt="2" colorScheme="indigo" onPress={handleLogin}>
-                        Sign in
+                  </Box>
+                </HStack>
+              )}
+              <FormControl id="email" isRequired>
+                <FormLabel>Email address</FormLabel>
+                <Input type="email" />
+              </FormControl>
+              <FormControl id="password" isRequired>
+                <FormLabel>Password</FormLabel>
+                <InputGroup>
+                  <Input type={showPassword ? "text" : "password"} />
+                  <InputRightElement h={"full"}>
+                    <Button
+                      variant={"ghost"}
+                      onClick={() =>
+                        setShowPassword((showPassword) => !showPassword)
+                      }
+                    >
+                      {showPassword ? <ViewIcon /> : <ViewOffIcon />}
                     </Button>
-                    <HStack mt="6" justifyContent="center">
-                        <Text
-                            fontSize="sm"
-                            color="coolGray.600"
-                            _dark={{
-                                color: "warmGray.200",
-                            }}
-                        >
-                            I&apos;m a new user.{" "}
-                        </Text>
-                        <Link
-                            _text={{
-                                color: "indigo.500",
-                                fontWeight: "medium",
-                                fontSize: "sm",
-                            }}
-                            href="#"
-                        >
-                            Sign Up
-                        </Link>
-                    </HStack>
-                </VStack>
-            </Box>
-        </Center>
-    );
+                  </InputRightElement>
+                </InputGroup>
+              </FormControl>
+              <Stack spacing={10} pt={2}>
+                <Button
+                  loadingText="Submitting"
+                  size="lg"
+                  bg={"blue.400"}
+                  color={"white"}
+                  _hover={{
+                    bg: "blue.500",
+                  }}
+                >
+                  {signUp ? "Sign Up" : "Login"}
+                </Button>
+              </Stack>
+              <Stack pt={6}>
+                <Text align={"center"}>
+                  Already a user?{" "}
+                  <Button
+                    color={"blue.400"}
+                    onClick={() => setSignUp((prevState) => !prevState)}
+                  >
+                    Login
+                  </Button>
+                </Text>
+              </Stack>
+            </Stack>
+          </Box>
+        </Stack>
+      </Flex>
+    </Layout>
+  );
 };
 
 export default Login;
