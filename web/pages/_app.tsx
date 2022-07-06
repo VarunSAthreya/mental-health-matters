e759bc9fd922dd28d197d8e868214f52c8adc53f
Varun S Athreya <varun.sathreya@gmail.com>
2022-07-06 23:57:07 +0530

    Added meta details, removed native base next adapter

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,4 +1,3 @@
-import { extendTheme, NativeBaseProvider } from "native-base";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../styles/theme";
@@ -6,6 +5,7 @@ import React from "react";
 import { useRouter } from "next/router";
 import ProtectedRoute from "../components/ProtectedRoute";
 import { AuthContextProvider } from "../hooks/auth";
+import Head from "next/head";
 
 const noAuthRequired = [
     "/",
@@ -21,19 +21,32 @@ function MyApp({ Component, pageProps }: AppProps) {
     const router = useRouter();
 
     return (
-      <ChakraProvider theme={theme}>
-        <NativeBaseProvider>
-          <AuthContextProvider>
-            {noAuthRequired.includes(router.pathname) ? (
-              <Component {...pageProps} />
-            ) : (
-              <ProtectedRoute>
-                <Component {...pageProps} />
-              </ProtectedRoute>
-            )}
-          </AuthContextProvider>
-        </NativeBaseProvider>
-      </ChakraProvider>
+        <>
+            <Head>
+                <title>Mental Health Matters</title>
+                <meta
+                    name="viewport"
+                    content="minimum-scale=1, initial-scale=1, width=device-width"
+                />
+                <meta
+                    name="description"
+                    content="Mental Health Matters is a mental health platform that helps you connect with people who are experiencing mental health issues."
+                />
+                <meta name="robots" content="all" />
+                <link rel="shortcut icon" href="/favicon.ico" />
+            </Head>
+            <ChakraProvider theme={theme}>
+                <AuthContextProvider>
+                    {noAuthRequired.includes(router.pathname) ? (
+                        <Component {...pageProps} />
+                    ) : (
+                        <ProtectedRoute>
+                            <Component {...pageProps} />
+                        </ProtectedRoute>
+                    )}
+                </AuthContextProvider>
+            </ChakraProvider>
+        </>
     );
 }
 
