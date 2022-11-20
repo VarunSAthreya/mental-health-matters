49482c67370821520cb47b8511474a664ca40277
Sandeep.M <sandeep.m24.rathnam@gmail.com>
2022-11-20 21:16:11 +0530

    Changed Theme

--- src/styles/theme.tsx
+++ src/styles/theme.tsx
@@ -1,16 +1,20 @@
 import { extendTheme } from "@chakra-ui/react";
 import { mode } from "@chakra-ui/theme-tools";
+import { StepsStyleConfig as Steps } from 'chakra-ui-steps';
 
 const theme = extendTheme({
+    components: {
+        Steps,
+    },
     styles: {
         global: (props: any) => ({
             body: {
-                bg: mode("#f8f9fa", "#111111")(props),
+                bg: mode('#E9EFF2', '#0D120E')(props),
             },
         }),
     },
     config: {
-        initialColorMode: "dark",
+        initialColorMode: 'dark',
         useSystemColorMode: false,
     },
 });
